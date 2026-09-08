c85a30e735f198bb5abc5f952946382654b8ddfb
arduinoenparques <usuario@Fmat_DelCubEle.inet.uady.mx>
2026-09-08 14:35:09 -0600

    inicio de programacion para 7 segmtos

---
+++ 7Segmentos/codigo7segmentos/codigo7segmentos.ino
@@ -0,0 +1,22 @@
+#define a 13
+#define b 12
+#define c 11
+#define d 10
+#define e 9
+#define f 8
+#define g 7
+
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+
+}
+
+void loop() {
+
+}
