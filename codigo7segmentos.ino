64c3760b5c3c13646738c27c558fcca1d29d23da
arduinoenparques <arduinoenparques@gmail.com>
2026-09-09 15:51:58 -0600

    continuacion

---
+++ codigo7segmentos.ino
@@ -0,0 +1,48 @@
+#define a 13
+#define b 12
+#define c 11
+#define d 10
+#define e 9
+#define f 8
+#define g 7
+
+#define lectura 6
+
+int valor=0;      //valor obtenido de la lectura
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+
+  pinMode(lectura,INPUT);
+}
+
+void espera()
+{
+  delay(1000);
+}
+
+void loop() {
+
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+
+  espera();
+
+  valor=digitalRead(lectura);
+
+
+}
