319055a3b6a7d8923ea99750ad1029050107da5f
arduinoenparques <275324072+arduinoenparques@users.noreply.github.com>
2026-09-10 14:23:24 -0600

    modificando con decoder

--- 7Segmentos/codigo7segmentos/codigo7segmentos.ino
+++ 7Segmentos/codigo7segmentos/codigo7segmentos.ino
@@ -2,9 +2,9 @@
 #define b 12
 #define c 11
 #define d 10
-#define e 9
-#define f 8
-#define g 7
+//#define e 9
+//#define f 8
+//#define g 7
 
 #define lectura A0
 
@@ -19,9 +19,9 @@ void setup() {
   pinMode(b,OUTPUT);
   pinMode(c,OUTPUT);
   pinMode(d,OUTPUT);
-  pinMode(e,OUTPUT);
-  pinMode(f,OUTPUT);
-  pinMode(g,OUTPUT);
+  //pinMode(e,OUTPUT);
+  //pinMode(f,OUTPUT);
+  //pinMode(g,OUTPUT);
 
   pinMode(lectura,INPUT);
 }
@@ -33,18 +33,18 @@ void espera()
 
 void loop() {
 
-  digitalWrite(a,HIGH);
+  /*digitalWrite(a,HIGH);
   digitalWrite(b,LOW);
   digitalWrite(c,LOW);
   digitalWrite(d,HIGH);
   digitalWrite(e,HIGH);
   digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
+  digitalWrite(g,HIGH);*/
 
   espera();
 
   valor=analogRead(lectura);// valor del ADC
    //Serial.println(valor);
    voltaje=valor*5/1024;
-    Serial.println(voltaje);
+  Serial.println(voltaje);
 }
