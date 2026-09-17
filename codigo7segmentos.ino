c621bbcb1819859dc6dfa5b9a3908a41b4b7e847
arduinoenparques <275324072+arduinoenparques@users.noreply.github.com>
2026-09-17 14:38:12 -0600

    uso de EEPROM

--- codigo7segmentos.ino
+++ codigo7segmentos.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 #define a 13
 #define b 12
 #define c 11
@@ -14,10 +16,12 @@ int d1;
 int valor=0;      //valor obtenido de la lectura
 int voltaje=0;
 
+int direccionInicial=0;
+
 void setup() {
 
   Serial.begin(9600);
-
+  EEPROM.begin();
   pinMode(a,OUTPUT);
   pinMode(b,OUTPUT);
   pinMode(c,OUTPUT);
@@ -42,6 +46,14 @@ void loop() {
   b1=bitRead(voltaje,1);
   c1=bitRead(voltaje,2);
   d1=bitRead(voltaje,3);
+
+  
+    EEPROM.write(direccionInicial,a1);
+    EEPROM.write(direccionInicial+1,b1);
+    EEPROM.write(direccionInicial+1,c1);
+    EEPROM.write(direccionInicial+1,d1);
+  
+  
    Serial.println(voltaje,BIN);
  
   Serial.println(String("valor en voltimetro ") + voltaje + String(" Input A del decoder= ") + a1);  
@@ -56,4 +68,10 @@ void loop() {
   digitalWrite(c,c1);
   digitalWrite(d,d1);
 
+Serial.println("---------------------------------------------------");
+  for(int x=0;x<4;x++){
+    Serial.println(String("El valor en la direccion ")+ x + String("es igual a->")+EEPROM.read(x));
+    }
+  Serial.println("---------------------------------------------------");
+  espera();
 }
